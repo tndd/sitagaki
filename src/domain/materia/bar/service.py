479fcb2d19ee17e1764a9c27b671dc67030511f1
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-24 04:06:49 +0900

    ユーザー側は抽象的な更新コマンドがあれば十分

---
+++ src/domain/materia/bar/service.py
@@ -0,0 +1,24 @@
+def update_bars(date_to: str):
+    """
+    条件:
+        何日地点までアップデートするかの日時。
+        TODO: ただし省略時は現在時刻とする。
+    効果:
+        データベースのbar情報を最新の状態に更新する。
+    """
+    # IMPL
+    pass
+
+
+### PRIVATE
+def update_bars_of_symbol(symbol: str, date_to: str):
+    """
+    条件:
+        - 更新対象のシンボル
+        - 何日まで更新するかの日付
+            TODO: ただし省略時は現在時刻とする。
+    効果:
+        指定シンボルのデータベース上のbar情報を更新する
+    """
+    # IMPL
+    pass
