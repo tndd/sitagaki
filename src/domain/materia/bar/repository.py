aa23ec2cc74e32bc7c0d97f81cae47c82a4f0248
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-24 03:46:59 +0900

    データが確実に取得できればいい。使用者は出所に関心を持つべきではない。

---
+++ src/domain/materia/bar/repository.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BarRepository:
+    def fetch_bars(symbol: str, start: str, end: str):
+        """
+        条件:
+            シンボルと開始日、終日を指定。
+        効果:
+            対象期間のローソク足をDBから取得し保存する。
+            もしもDBにデータが存在しなければ、オンライン上からの取得を試みる。
+        """
+        pass
+
+
+    ### PRIVATE
+    def pull_bars_from_online(symbol: str, start: str, end: str):
+        """
+        条件:
+            シンボルと開始日、終日を指定。
+        効果:
+            対象期間のローソク足をオンライン上から取得し保存する。
+        """
+        pass
+
+
+    def fetch_bars_from_local(symbol: str, start: str, end: str):
+        """
+        条件:
+            シンボルと開始日、終日を指定。
+        効果:
+            対象期間のローソク足をDB上から取得し保存する。
+        """
+        pass
