e1ce9a0729d2e80e468b631a4efcdfb490371d4d
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-25 03:45:49 +0900

    関数の入り口と出口は自前で定義したモデルか汎用的なデータ型に統一する。

--- src/infra/api/alpaca/historical.py
+++ src/infra/api/alpaca/historical.py
@@ -2,9 +2,9 @@ from os import getenv
 from typing import List
 
 from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.models.bars import Bar as AlpcBar
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame as AlpcTimeFrame
-from pydantic import BaseModel
 
 
 class Timeframe(AlpcTimeFrame):
@@ -21,6 +21,25 @@ class Timeframe(AlpcTimeFrame):
     pass
 
 
+class Bar(AlpcBar):
+    """
+    AlpacaのBarモデルをラッピングしたもの。
+    このモデルはBaseModelを継承したものである。
+
+    要素:
+        - symbol (str): バーを形成するのティッカー識別子。
+        - timestamp (datetime): バーの終了タイムスタンプ。
+        - open (float): 期間の開始価格。
+        - high (float): 期間中の高値。
+        - low (float): 期間中の安値。
+        - close (float): 期間の終値。
+        - volume (float): 期間で取引されたボリューム。
+        - trade_count (Optional[float]): 発生した取引数。
+        - vwap (Optional[float]): ボリューム加重平均価格。
+        - exchange (Optional[float]): バーが形成された取引所。
+    """
+
+
 def create_stock_historical_data_client() -> StockHistoricalDataClient:
     return StockHistoricalDataClient(
         api_key=getenv('APCA_KEY'),
@@ -32,24 +51,10 @@ def get_bars(
     symbol: str,
     timeframe: Timeframe,
     start: str = '2000-01-01'
-) -> List[BaseModel]:
+) -> List[Bar]:
     """
     日足のヒストリカルバー情報を取得。
     デフォルトの開始日は2000年元年とする。
-
-    戻り値について:
-        * 条件に合致するBar(BaseModel)のリスト
-        * Barを構成する情報
-            - symbol (str): バーを形成するのティッカー識別子。
-            - timestamp (datetime): バーの終了タイムスタンプ。
-            - open (float): 期間の開始価格。
-            - high (float): 期間中の高値。
-            - low (float): 期間中の安値。
-            - close (float): 期間の終値。
-            - volume (float): 期間で取引されたボリューム。
-            - trade_count (Optional[float]): 発生した取引数。
-            - vwap (Optional[float]): ボリューム加重平均価格。
-            - exchange (Optional[float]): バーが形成された取引所。
     """
     cli = create_stock_historical_data_client()
     rq = StockBarsRequest(
@@ -58,4 +63,4 @@ def get_bars(
         start=start
     )
     bars = cli.get_stock_bars(rq)
-    return list(bars.data.values())[0]
+    return bars.data[symbol]
