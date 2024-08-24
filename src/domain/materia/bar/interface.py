aa5f2eb48c34e41a72395ca71f658c543b4c5d1b
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-24 16:53:56 +0900

    デフォルト引数の追加
    service層ではデフォルト引数は使わないようにする。
    引数の管理は一貫してinterface層で行う方針で

--- src/domain/materia/bar/interface.py
+++ src/domain/materia/bar/interface.py
@@ -1,16 +1,17 @@
+from datetime import datetime
+
 from domain.materia.bar.repository import BarRepository
 
 
 def fetch_bars(
         rp: BarRepository,
         symbol: str,
-        start: str = 'DEFAULT_START',
-        end: str = 'NOW'
+        start: str = '2000-01-01',
+        end: str = datetime.now().strftime('%Y-%m-%d')
     ):
     """
     条件:
         シンボルと開始日、終日を指定。
-        # TODO startとenのデフォルト引数
     戻り値:
         DF
     効果:
@@ -20,11 +21,12 @@ def fetch_bars(
     pass
 
 
-def update_bars(date_to: str):
+def update_bars(
+        date_to: str = datetime.now().strftime('%Y-%m-%d')
+    ):
     """
     条件:
         何日地点までアップデートするかの日時。
-        TODO: ただし省略時は現在時刻とする。
     効果:
         データベースのbar情報を最新の状態に更新する。
     """
--- src/domain/materia/bar/service.py
+++ src/domain/materia/bar/service.py
@@ -10,7 +10,6 @@ def update_bars_of_symbol(symbol: str, date_to: str):
     条件:
         - 更新対象のシンボル
         - 何日まで更新するかの日付
-            TODO: ただし省略時は現在時刻とする。
     効果:
         指定シンボルのデータベース上のbar情報を更新する
     """
