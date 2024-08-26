6e2aa60e72a8527ec8577667a2ba21d25e8fb89b
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-26 12:21:24 +0900

    sessionの追加

--- src/infra/db/sqlmodel.py
+++ src/infra/db/sqlmodel.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy.engine import Engine
+from sqlalchemy.engine.base import Engine
 from sqlmodel import Session, SQLModel, select
 
 
@@ -8,12 +8,15 @@ from sqlmodel import Session, SQLModel, select
 class SqlModelClient:
     engine: Engine
 
+    def session(self) -> Session:
+        return Session(self.engine)
+
     def insert_models(self, models: list[SQLModel]):
-        with Session(self.engine) as session:
+        with self.session() as session:
             session.add_all(models)
             session.commit()
 
-    def select_models(self, conditions: dict) -> list[SQLModel]:
-        with Session(self.engine) as session:
-            stmt = select(SQLModel).filter_by(**conditions)
+    def select_models(self, model_class: type[SQLModel], conditions: dict) -> list[SQLModel]:
+        with self.session() as session:
+            stmt = select(model_class).filter_by(**conditions)
             return session.exec(stmt).all()
