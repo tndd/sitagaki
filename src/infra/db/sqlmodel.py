a002f7c912047da77d6095529fa36feea52b268e
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-26 18:19:14 +0900

    test_select_modelsのテスト１までを実装

--- src/infra/db/sqlmodel.py
+++ src/infra/db/sqlmodel.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from sqlalchemy.engine.base import Engine
 from sqlmodel import Session, SQLModel, select
@@ -16,7 +17,9 @@ class SqlModelClient:
             session.add_all(models)
             session.commit()
 
-    def select_models(self, model_class: type[SQLModel], conditions: dict) -> list[SQLModel]:
+    def select_models(self, model_class: type[SQLModel], conditions: Optional[dict] = None) -> list[SQLModel]:
         with self.session() as session:
-            stmt = select(model_class).filter_by(**conditions)
+            stmt = select(model_class)
+            if conditions:
+                stmt = stmt.filter_by(**conditions)
             return session.exec(stmt).all()
--- tests/infra/db/test_sqlmodel.py
+++ tests/infra/db/test_sqlmodel.py
@@ -6,6 +6,20 @@ from infra.db.sqlmodel import SqlModelClient
 from tests.factory.user import User, generate_sample_users
 
 
+def assert_users_equal(db_users, original_users):
+    # 件数確認
+    assert len(db_users) == len(original_users)
+    # 内容確認: nameでソートし１件ずつ突合
+    db_users_sorted = sorted(db_users, key=lambda u: u.name)
+    original_users_sorted = sorted(original_users, key=lambda u: u.name)
+    for db_user, original_user in zip(db_users_sorted, original_users_sorted):
+        assert db_user.id is not None
+        assert db_user.created_at == original_user.created_at
+        assert db_user.name == original_user.name
+        assert db_user.email == original_user.email
+        assert db_user.credit == original_user.credit
+
+
 def test_insert_models(test_engine):
     cli = SqlModelClient(test_engine)
     # テーブルの作成
@@ -19,14 +33,49 @@ def test_insert_models(test_engine):
     with cli.session() as session:
         # データ取得
         db_users = session.exec(select(User)).all()
-        # 件数確認
-        assert len(db_users) == len(users)
-        # 内容確認: idでソートし１件ずつ突合
-        db_users_sorted = sorted(db_users, key=lambda u: u.name)
-        users_sorted = sorted(users_copy_for_valid, key=lambda u: u.name)
-        for db_user, original_user in zip(db_users_sorted, users_sorted):
-            assert db_user.id is not None
-            assert db_user.created_at == original_user.created_at
-            assert db_user.name == original_user.name
-            assert db_user.email == original_user.email
-            assert db_user.credit == original_user.credit
+        assert_users_equal(db_users, users_copy_for_valid)
+
+
+def test_select_models(test_engine):
+    cli = SqlModelClient(test_engine)
+    # テスト用データの登録
+    SQLModel.metadata.create_all(test_engine)
+    users = generate_sample_users()
+    users_copy_for_valid = deepcopy(users)
+    cli.insert_models(users)
+    """
+    テスト１:
+    条件なしのselect
+    内容がusersと一致するかの確認。
+
+    select_modelsが機能していることと、
+    conditionを渡さなくても問題ないことを確認
+    """
+    db_users = cli.select_models(User)
+    assert_users_equal(db_users, users_copy_for_valid)
+    """
+    テスト２:
+    日付範囲のテスト
+    - 2000年未満 -> nagisa,kazusa,aliceのデータが取得される
+    - 2000年以降 -> astar,helta,carol,david
+    - 2000年 ~ 2005年 -> astar,helta,carol
+    """
+    """
+    テスト３:
+    creditによる絞り込み
+    - 100 ~ 500 -> helta,carol,david
+    - 10000以上 -> nagisa,astar
+    """
+    """
+    テスト4:
+    メールアドレスによる絞り込み
+    - @blmail -> nagisa,kazusa,alice
+    - @stmail -> astar,helta
+    - @blmail.tri -> nagisa,kazusa
+    """
+    """
+    テスト5:
+    複合条件による絞り込み
+    - @blmailかつcredit10000以上 -> nagisa
+    - @blmailかつ日付が1981以降 -> kazusa,alice
+    """
