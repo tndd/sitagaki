586cd5feccbe6c0b51f7c68ccef146011a4c893f
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-29 06:01:26 +0900

    alpaca bar tableのテスト雛形
    だが何をテストするかな

--- tests/infra/db/table/test_alpaca.py
+++ tests/infra/db/table/test_alpaca.py
@@ -1,3 +1,14 @@
 from infra.db.table.alpaca import TblBarDay, TblBarHour, TblBarMin
 
-# IMPL
+
+# テーブル作成テスト
+def test_tbl_bar_min():
+    pass
+
+
+def test_tbl_bar_hour():
+    pass
+
+
+def test_tbl_bar_day():
+    pass
