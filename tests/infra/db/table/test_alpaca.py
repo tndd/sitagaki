8c70d8747b19a31aaf9ece3a8df0903bfcf780ff
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-08-29 06:07:30 +0900

    作業メモ追加

--- tests/infra/db/table/test_alpaca.py
+++ tests/infra/db/table/test_alpaca.py
@@ -1,14 +1,21 @@
 from infra.db.table.alpaca import TblBarDay, TblBarHour, TblBarMin
 
+"""
+# Note: 作業メモ
 
-# テーブル作成テスト
-def test_tbl_bar_min():
+とりあえずテーブルができているかの確認だけでもするか？
+本当はデータの挿入や確認というテストが必要だが、
+そのためにはまずBarモデルをTblBar*に変換するアダプタを用意しないと。
+それとBarのfactoryも。
+"""
+
+def test_tbl_bar_min(test_sqlm_cli):
     pass
 
 
-def test_tbl_bar_hour():
+def test_tbl_bar_hour(test_sqlm_cli):
     pass
 
 
-def test_tbl_bar_day():
+def test_tbl_bar_day(test_sqlm_cli):
     pass
