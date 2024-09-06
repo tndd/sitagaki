fea9ef85dbabb8a70a37958b123818465f012271
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-06 16:59:12 +0900

    概形

--- infra/db/sqlmodel.py
+++ infra/db/sqlmodel.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List
 
 from sqlalchemy.engine.base import Engine
-from sqlmodel import Session, SQLModel, select
+from sqlmodel import Session, SQLModel
 
 
 @dataclass
@@ -17,41 +17,11 @@ class SqlModelClient:
             session.add_all(models)
             session.commit()
 
-    def select_models(
-        self,
-        model: type[SQLModel],
-        conditions: Optional[dict] = None
-    ) -> List[SQLModel]:
+
+    def select_models(self, stmt: SQLModel) -> List[SQLModel]:
         """
-        NOTE: stmtを直接渡すシンプルな形式に変える？
-
-            modelとconditionsを別々に渡す形式である必要があまりない気がしてきた。
-            なぜならconditionsを作る時点でmodelが密接に関連しているからだ。
-
-            だったらstmtのリストを渡す形にして、それをそのままsession管理しつつ実行、
-            という形のほうが機能として合理的ではないだろうか。
-            今の実装であればこれをリポジトリから呼び出した場合、
-            インフラ層のモデルをリポジトリが意識してしまう状態となるためマズい。
-
-            stmtの作成は、infra/db/queryあたりに集めてそれを呼び出す形とすればいい。
-            そうすればリポジトリ側からインフラ側の実装を秘匿できる上、
-            気にするのはstmtの条件の数値だけで済む。
-
-        懸念:
-            今の所この関数はselectで使うという想定となっている。
-            だがこの関数の機能が実行というところに注力して作られているならば、
-            これがselectにしか使われないということはないのではないだろうか？
-
-            > 1:
-                ありそうなケースはdelete,updateしてからselectみたいなケース。
-                着地地点はselectというルールでこれを設計するならば、
-                戻り値はこのままのall()でいいのではないか。
-
-                それにupdateやdeleteで終わってall()の結果が帰ったとしても、
-                それを使わなければいいだけの話で動作に支障はない。
+        selectステートメントを実行し、モデルのリストを返す。
         """
         with self.session() as session:
-            stmt = select(model)
-            if conditions:
-                stmt = stmt.filter_by(**conditions)
-            return session.exec(stmt).all()
+            result = session.exec(stmt).all()
+        return result
