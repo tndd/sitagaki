688f4f2c127f7b7db7ef4bd5467833f7941ffd28
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-07 02:30:19 +0900

    case1の実装

--- tests/infra/db/stmt/materia/test_bar.py
+++ tests/infra/db/stmt/materia/test_bar.py
@@ -1,11 +1,36 @@
-def test_get_stmt_select_bar():
-    """
-    Note: いかにstmtの内容を検証するか
+from datetime import datetime
 
-    stmtの内容の正誤を検証するには、
-    やっぱり実際にstmtを実行してみるしかないか。
+from domain.materia.bar.model import Timeframe
+from infra.adapter.materia.bar import adapt_to_tbl_bar_alpaca_list
+from infra.db.stmt.materia.bar import get_stmt_select_bar
+from tests.utils.factory.domain.materia.bar import generate_bar_list
+
+
+def test_get_stmt_select_bar(test_sqlm_cli):
+    # 検証用のbarのリストをDBに用意
+    bars = generate_bar_list()
+    tbl_bars = adapt_to_tbl_bar_alpaca_list(bars, Timeframe.DAY)
+    test_sqlm_cli.insert_models(tbl_bars)
     """
-    # 検証用のbarのリストをDBに用意しておく
-    # テスト用のstmtを用意し実行
-    # 結果の検証
+    case1: シンボルのみによる絞り込み
+
+    期待される結果:
+        - シンボルが"AAPL"のbarのみ取得
+        - 取得件数は３件
+    """
+    stmt_1 = get_stmt_select_bar(
+        symbol="AAPL",
+        timeframe=Timeframe.DAY
+    )
+    bars_result_1 = test_sqlm_cli.select_models(stmt_1)
+    assert len(bars_result_1) == 3
+    assert all(bar.symbol == "AAPL" for bar in bars_result_1)
+
+    """
+    テスト2: シンボルと時間軸による絞り込み
+
+    期待される結果:
+        - シンボルが"AAPL"のbarのみ取得
+        - 取得件数は３件
+    """
     pass
