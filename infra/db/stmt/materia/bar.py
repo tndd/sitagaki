ad47eecf965e100579aac6589a7f6886d42bc788
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-06 18:41:09 +0900

    select_barというstmt関数の追加
    テストは未実装

--- infra/db/stmt/materia/bar.py
+++ infra/db/stmt/materia/bar.py
@@ -1,2 +1,33 @@
-def select_bar():
-    pass
+from datetime import datetime
+
+from sqlmodel import between, select
+from sqlmodel.sql.expression import SelectOfScalar
+
+from domain.materia.bar.model import Timeframe
+from infra.db.table.bar import (TblBarDayAlpaca, TblBarHourAlpaca,
+                                TblBarMinAlpaca)
+
+
+def select_bar(
+        symbol: str,
+        timeframe: str,
+        start: datetime,
+        end: datetime
+) -> SelectOfScalar:
+    """
+    指定されたtimeframeの
+    特定のシンボルのstart~endの間のbarデータを取得する。
+    """
+    bar_model = None
+    if timeframe == Timeframe.MIN:
+        bar_model = TblBarMinAlpaca
+    elif timeframe == Timeframe.HOUR:
+        bar_model = TblBarHourAlpaca
+    elif timeframe == Timeframe.DAY:
+        bar_model = TblBarDayAlpaca
+    else:
+        raise ValueError(f"Invalid timeframe: {timeframe}")
+    stmt = select(bar_model) \
+        .where(bar_model.symbol == symbol) \
+        .where(between(bar_model.timestamp, start, end))
+    return stmt
