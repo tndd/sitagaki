99ba97a4ce31c960581124af42188faafd398b36
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-07 02:36:02 +0900

    case2の実装&コメント追加

--- tests/infra/db/stmt/materia/test_bar.py
+++ tests/infra/db/stmt/materia/test_bar.py
@@ -14,6 +14,9 @@ def test_get_stmt_select_bar(test_sqlm_cli):
     """
     case1: シンボルのみによる絞り込み
 
+    条件:
+        - シンボルが"AAPL"
+
     期待される結果:
         - シンボルが"AAPL"のbarのみ取得
         - 取得件数は３件
@@ -27,10 +30,27 @@ def test_get_stmt_select_bar(test_sqlm_cli):
     assert all(bar.symbol == "AAPL" for bar in bars_result_1)
 
     """
-    テスト2: シンボルと時間軸による絞り込み
+    case2: シンボルと時間軸による絞り込み
+
+    条件:
+        - シンボルが"AAPL"
+        - 時間軸が"DAY"
+        - 日付が2024-01-02から2024-01-04の間
 
     期待される結果:
         - シンボルが"AAPL"のbarのみ取得
-        - 取得件数は３件
+        - 日付が2024-01-02から2024-01-04の間のbarのみ取得
+        - 取得件数は以下の日付の2件
+            - timestamp=datetime(2024, 1, 2, 5, 0, 0)
+            - timestamp=datetime(2024, 1, 3, 5, 0, 0)
     """
-    pass
+    stmt_2 = get_stmt_select_bar(
+        symbol="AAPL",
+        timeframe=Timeframe.DAY,
+        start=datetime(2024, 1, 2),
+        end=datetime(2024, 1, 4)
+    )
+    bars_result_2 = test_sqlm_cli.select_models(stmt_2)
+    assert len(bars_result_2) == 2
+    assert all(bar.symbol == "AAPL" for bar in bars_result_2)
+    assert all(datetime(2024, 1, 2) <= bar.timestamp <= datetime(2024, 1, 4) for bar in bars_result_2)
