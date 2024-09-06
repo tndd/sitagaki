c7027d6cf99a5ed44b6c850392062efe082e88af
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-07 00:33:23 +0900

    generate_bar_listとテストの追加

--- tests/factory/domain/materia/test_bar.py
+++ tests/factory/domain/materia/test_bar.py
@@ -1,7 +1,14 @@
 from domain.materia.bar.model import Bar
-from tests.utils.factory.domain.materia.bar import generate_bar
+from tests.utils.factory.domain.materia.bar import (generate_bar,
+                                                    generate_bar_list)
 
 
 def test_generate_bar():
     bar = generate_bar()
     assert isinstance(bar, Bar)
+
+
+def test_generate_bar_list():
+    bars = generate_bar_list()
+    assert isinstance(bars, list)
+    assert all(isinstance(bar, Bar) for bar in bars)
--- tests/utils/factory/domain/materia/bar.py
+++ tests/utils/factory/domain/materia/bar.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from domain.materia.bar.model import Bar
 
@@ -15,3 +16,68 @@ def generate_bar() -> Bar:
         trade_count=1009074.0,
         vwap=185.937347
     )
+
+
+def generate_bar_list() -> List[Bar]:
+    """
+    AAPL,MSFTの日足のデータ
+    * AAPLについては、2024/1/2~2024/1/4の3日間。
+    * MSFTについては、2024/1/2~2024/1/3の2日間。
+    """
+    return [
+        Bar(
+            symbol="AAPL",
+            timestamp=datetime(2024, 1, 2, 5, 0, 0),
+            open=187.15,
+            high=188.44,
+            low=183.885,
+            close=185.64,
+            volume=82496943.0,
+            trade_count=1009074.0,
+            vwap=185.937347
+        ),
+        Bar(
+            symbol="AAPL",
+            timestamp=datetime(2024, 1, 3, 5, 0, 0),
+            open=184.22,
+            high=185.88,
+            low=183.43,
+            close=184.25,
+            volume=58418916.0,
+            trade_count=656956.0,
+            vwap=184.322631
+        ),
+        Bar(
+            symbol="AAPL",
+            timestamp=datetime(2024, 1, 4, 5, 0, 0),
+            open=182.15,
+            high=183.0872,
+            low=180.88,
+            close=181.91,
+            volume=71992243.0,
+            trade_count=712850.0,
+            vwap=182.01753
+        ),
+        Bar(
+            symbol="MSFT",
+            timestamp=datetime(2024, 1, 2, 5, 0, 0),
+            open=181.99,
+            high=182.76,
+            low=180.17,
+            close=181.18,
+            volume=62339687.0,
+            trade_count=681262.0,
+            vwap=181.474375
+        ),
+        Bar(
+            symbol="MSFT",
+            timestamp=datetime(2024, 1, 3, 5, 0, 0),
+            open=182.085,
+            high=185.6,
+            low=181.5,
+            close=185.56,
+            volume=59151720.0,
+            trade_count=669304.0,
+            vwap=184.370323
+        )
+    ]
