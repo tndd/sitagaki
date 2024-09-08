a90fccc4cb097b567d955b923ad97d177d64e04b
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-08 23:37:16 +0900

    Revert "WARN: 一時的にログ出力をするように変更"
    This reverts commit f31f79763cb659e5dfa08fdbb0fe2d31d7feb0b8.
    
    とりあえず元に戻す。
    正しいログ出力方法を調べないといけない。

--- tests/infra/api/alpaca/test_trading.py
+++ tests/infra/api/alpaca/test_trading.py
@@ -1,28 +1,11 @@
-from datetime import datetime
-
 import pytest
 from alpaca.trading.models import Asset
-from loguru import logger
 
 from infra.api.alpaca.trading import get_assets
 
-# ログの設定
-current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-log_file_path = f"tests/logs/alpaca_assets_{current_time}.log"
-logger.add(log_file_path)
 
-
-# @pytest.mark.online
+@pytest.mark.online
 def test_get_assets():
     assets = get_assets()
     assert isinstance(assets, list)
     assert all(isinstance(asset, Asset) for asset in assets)
-    # TSVヘッダーをログに記録
-    header = "id\tasset_class\texchange\tsymbol\tname\tstatus\ttradable\tmarginable\tshortable\teasy_to_borrow\tfractionable\tmin_order_size\tmin_trade_increment\tprice_increment\tmaintenance_margin_requirement"
-    logger.info(header)
-    # assetsの内容をログに記録
-    for asset in assets:
-        log_line = f"{asset.id}\t{asset.asset_class}\t{asset.exchange}\t{asset.symbol}\t{asset.name}\t{asset.status}\t{asset.tradable}\t{asset.marginable}\t{asset.shortable}\t{asset.easy_to_borrow}\t{asset.fractionable}\t{asset.min_order_size}\t{asset.min_trade_increment}\t{asset.price_increment}\t{asset.maintenance_margin_requirement}"
-        logger.info(log_line)
-    # 取得したアセットの総数をログに記録
-    logger.info(f"取得したアセットの総数: {len(assets)}")
