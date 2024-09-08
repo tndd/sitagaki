c2f90e4bacaf3638997ced94ad0c30775c0458fc
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-08 16:35:10 +0900

    test_get_barsという総合テスト追加
    ２重で通信が走ることになるが、少なくともこの程度であればテストカバレッジを増やすことを優先して問題ないだろう。

--- tests/infra/api/alpaca/test_historical.py
+++ tests/infra/api/alpaca/test_historical.py
@@ -6,26 +6,39 @@ from infra.api.alpaca.historical import extract_bar_alpaca_list_from_barset, get
 from tests.utils.factory.infra.api.alpaca import generate_barset
 
 
+@pytest.mark.ext
 def test_get_bars():
     """
-    TODO: テスト実装
+    MEMO: barsの総合テストは冗長か？
         get_barsの動作はかなりget_barsetと被ってるから、
         barsetのテストを省略するべきだろうか？
 
         外部で使われるget_barsだけでいいだろう。
         test_get_barsetやtest_extract_bar_alpaca_list_from_barsetは
         その部品でしかないのだから、その品質はこの総合テストで担保されるべきもの。
+
+        仮に外部との通信というコストを度外視できるなら、
+        全関数についてテストを行いカバレッジ１００％を目指すべきだろう。
+        そして外部との通信があるとはいえ負荷は最小限。
+        それならこのテストを追加するくらい問題ないんじゃないか？
+        こういう議論は通信容量が大容量で問題が実際に起こり始めた段階で行うべきではないか。
     """
-    pass
+    barset = get_barset(
+        symbol='AAPL',
+        start='2024-01-01',
+        timeframe=TimeFrameAlpaca.Day
+    )
+    bars = extract_bar_alpaca_list_from_barset(barset)
+    assert isinstance(bars, list)
+    assert all(isinstance(bar, Bar) for bar in bars)
 
 
 @pytest.mark.ext
 def test_get_barset():
-    timeframe = TimeFrameAlpaca.Day
     barset = get_barset(
         symbol='AAPL',
         start='2024-01-01',
-        timeframe=timeframe
+        timeframe=TimeFrameAlpaca.Day
     )
     # NOTE: 将来的にはログなどの方法で中身を確認する方針に変更
     # 出力検証用
@@ -43,7 +56,7 @@ def test_get_barset():
 
 def test_extract_bar_alpaca_list_from_barset():
     """
-    BarSetの中からBarのリストを取り出すアダプタのテスト
+    BarSetの中からBarのリストを取り出す機能のテスト
     """
     barset = generate_barset()
     bars = extract_bar_alpaca_list_from_barset(barset)
