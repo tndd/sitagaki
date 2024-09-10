c17f00d1efb4a57d30817881cd608f96b550acac
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-11 01:39:27 +0900

    test_adapt_bar_list_sqlm_to_domainの修正

--- tests/infra/adapter/materia/test_bar.py
+++ tests/infra/adapter/materia/test_bar.py
@@ -1,7 +1,6 @@
-from alpaca.data.models import Bar
 from alpaca.data.timeframe import TimeFrame as TimeFrameAlpaca
 
-from domain.materia.bar.model import Timeframe
+from domain.materia.bar.model import Bar, Timeframe
 from infra.adapter.materia.bar import (
     adapt_bar_alpaca_list_to_sqlm,
     adapt_bar_domain_to_sqlm,
@@ -15,6 +14,7 @@ from tests.utils.factory.infra.api.alpaca import (
     generate_bar_alpaca,
     generate_bar_alpaca_list,
 )
+from tests.utils.factory.infra.db.table.bar import generate_tbl_bar_base_list
 
 
 def test_adapt_timeframe_domain_to_alpaca():
@@ -47,10 +47,8 @@ def test_adapt_bar_list_domain_to_sqlm():
 
 
 def test_adapt_bar_list_sqlm_to_domain():
-    # FIXME: テストが通ってないので終了させてる
-    return
     # bars_sqlmの用意
-    # TODO: alpaca_sqls_listのファクトリを作る
+    bars_sqlm_list = generate_tbl_bar_base_list()
     # adapt_bar_list_sqlm_to_domainによる変換
     bars = adapt_bar_list_sqlm_to_domain(bars_sqlm_list)
     # 変換結果のチェック
