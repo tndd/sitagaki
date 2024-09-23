ed7702db29cc3def309f52d85a48b7b4b9b52663
tau <36781951+tndd@users.noreply.github.com>
2024-09-23 23:50:55 +0900

    test_table_bar_alpaca_listの実装
    簡易的に絞り込みのテストも行っておく

--- tests/infra/db/table/test_bar.py
+++ tests/infra/db/table/test_bar.py
@@ -1,5 +1,14 @@
 from infra.db.table.bar import TableBarAlpaca
-from tests.utils.factory.infra.db.table.bar import generate_table_bar_alpaca
+from tests.utils.factory.infra.db.table.bar import (
+    generate_table_bar_alpaca,
+    generate_table_bar_alpaca_list,
+)
+
+"""
+MEMO:
+    基本的にはTableBarAlpacaがテーブルクラスとして機能しているかを確認する。
+    だがその他にも挿入や取得などの一通りの動作確認も行っておく。
+"""
 
 
 def test_table_bar_alpaca_is_created(test_peewee_cli):
@@ -12,3 +21,31 @@ def test_table_bar_alpaca_is_created(test_peewee_cli):
     test_peewee_cli.insert_models([table_bar_alpaca])
     # テーブルの存在確認
     assert TableBarAlpaca.table_exists()
+
+
+def test_table_bar_alpaca_list(test_peewee_cli):
+    """
+    TableBarAlpacaのリストを投入および取得の基本テスト。
+
+    1. データが投入されたことを確認
+    2. synbol=AAPL and timeframe=1 and adjustment = 1 のデータ取得
+        ３件のデータが取得されることを確認
+    3. adjustment=2のデータ取得
+        ０件のデータが取得されることを確認
+    """
+    # 投入用のTableBarAlpacaを作成
+    table_bar_alpaca_list = generate_table_bar_alpaca_list()
+    # テーブルに投入
+    test_peewee_cli.insert_models(table_bar_alpaca_list)
+    # 1. データが投入されたことを確認
+    assert len(TableBarAlpaca.select()) == len(table_bar_alpaca_list)
+    # 2. symbol=AAPL and timeframe=1 and adjustment = 1 のデータ取得
+    assert len(TableBarAlpaca.select().where(
+        TableBarAlpaca.symbol == "AAPL",
+        TableBarAlpaca.timeframe == 4,
+        TableBarAlpaca.adjustment == 1,
+    )) == 3
+    # 3. adjustment=2のデータ取得
+    assert len(TableBarAlpaca.select().where(
+        TableBarAlpaca.adjustment == 2,
+    )) == 0
