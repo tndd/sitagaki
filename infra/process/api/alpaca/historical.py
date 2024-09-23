b00cd319104acd60bb0b14dca3fc91876c55c751
tau <36781951+tndd@users.noreply.github.com>
2024-09-24 04:06:47 +0900

    コメントのrename

--- infra/process/api/alpaca/historical.py
+++ infra/process/api/alpaca/historical.py
@@ -5,7 +5,7 @@ from alpaca.data.models import BarSet as BarSetAlpacaApi
 
 from domain.materia.bar.model import Bar, Timeframe
 from infra.adapter.materia.bar import adapt_bar_domain_to_sqlm
-from infra.db.table.bar import TblBarBase
+from infra.db.table.bar import TableBarAlpaca
 
 
 def extract_bar_list_alpaca_api_from_barset(barset: BarSetAlpacaApi) -> List[BarAlpacaApi]:
@@ -18,16 +18,16 @@ def extract_bar_list_alpaca_api_from_barset(barset: BarSetAlpacaApi) -> List[Bar
 def convert_bar_list_alpaca_api_to_table(
         bars_alpaca_api: List[BarAlpacaApi],
         timeframe: Timeframe
-) -> List[TblBarBase]:
+) -> List[TableBarAlpaca]:
     """
     alpacaのバーのリストをDBのバーのリストに変換する。
 
-    HACK: bar_alpaca -> bar_sqlmへの変換過程
+    HACK: bar_alpaca_api -> bar_alpaca_tableへの変換過程
         本来であれば、
-        "alpaca -> domain -> sqlm"の順で変換が必要だが、
-        "alpaca -> sqlm"という直接変換を行う形式となってしまっている。
+        "alpaca_api -> domain -> table"の順で変換が必要だが、
+        "alpaca_api -> table"という直接変換を行う形式となってしまっている。
 
-        たまたま、alpacaのバーとdomainのバーは同じデータ構造をしているため、
+        たまたま、alpaca_apiのバーとdomainのバーは同じデータ構造をしているため、
         このような変換が可能になっている。
     """
     return [adapt_bar_domain_to_sqlm(bar, timeframe) for bar in bars_alpaca_api]
