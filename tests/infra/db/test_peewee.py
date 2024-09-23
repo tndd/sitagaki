667895338ca200a2d1805ce9d0f927906810c02a
tau <36781951+tndd@users.noreply.github.com>
2024-09-23 21:30:34 +0900

    dbだけほしいということはないだろう

--- tests/infra/db/test_peewee.py
+++ tests/infra/db/test_peewee.py
@@ -9,11 +9,10 @@ class User(PeeweeTable):
     email = CharField()
 
 
-def test_insert_models(test_peewee_db):
+def test_insert_models(test_peewee_cli):
     """
     ３件のデータを投入し、その内容を確認する。
     """
-    peewee_client = PeeweeClient(test_peewee_db)
     # 挿入するユーザーインスタンスのリストを作成
     users = [
         User(username='user1', email='user1@example.com'),
@@ -21,7 +20,7 @@ def test_insert_models(test_peewee_db):
         User(username='user3', email='user3@example.com')
     ]
     # ユーザーデータ挿入
-    peewee_client.insert_models(users)
+    test_peewee_cli.insert_models(users)
     # 挿入されたユーザーを取得
     retrieved_users = User.select()
     # 取得したユーザーを検証
@@ -34,7 +33,7 @@ def test_insert_models(test_peewee_db):
     assert retrieved_users[2].email == 'user3@example.com'
 
 
-def test_insert_models_multiple(test_peewee_db):
+def test_insert_models_multiple(test_peewee_cli):
     """
     複数回にわたって挿入する場合のテスト
 
@@ -45,7 +44,6 @@ def test_insert_models_multiple(test_peewee_db):
             * さらに3件のデータが挿入され、合計6件。
             * usernameが"user1"のデータが2件。
     """
-    peewee_client = PeeweeClient(test_peewee_db)
     # 挿入するユーザーインスタンスのリストを作成
     users = [
         User(username='user1', email='user1@example.com'),
@@ -53,11 +51,11 @@ def test_insert_models_multiple(test_peewee_db):
         User(username='user3', email='user3@example.com')
     ]
     # 1回目の投入
-    peewee_client.insert_models(users)
+    test_peewee_cli.insert_models(users)
     retrieved_users = User.select()
     assert len(retrieved_users) == 3
     # 2回目の投入
-    peewee_client.insert_models(users)
+    test_peewee_cli.insert_models(users)
     retrieved_users = User.select()
     assert len(retrieved_users) == 6
     # さらにテーブル内容を確認する
--- tests/utils/fixture/infra/db/peewee.py
+++ tests/utils/fixture/infra/db/peewee.py
@@ -1,12 +1,12 @@
 import pytest
 from peewee import SqliteDatabase
 
-from infra.db.peewee import DB_PROXY
+from infra.db.peewee import DB_PROXY, PeeweeClient
 
 
 @pytest.fixture
-def test_peewee_db():
+def test_peewee_cli():
     test_db = SqliteDatabase(':memory:')
     DB_PROXY.initialize(test_db)
-    yield test_db
+    yield PeeweeClient(test_db)
     test_db.close()
