fbeb58ceca0a460faa8824c5a76225ad16a6676a
tau <36781951+tndd@users.noreply.github.com>
2024-09-24 19:58:41 +0900

    del imports

--- tests/infra/adapter/materia/bar/test_process.py
+++ tests/infra/adapter/materia/bar/test_process.py
@@ -1,15 +1,7 @@
 from alpaca.data.models import Bar
 
-from domain.materia.bar.model import Timeframe
-from infra.adapter.materia.bar.process import (
-    convert_bar_list_alpaca_api_to_table,
-    extract_bar_list_alpaca_api_from_barset,
-)
-from infra.db.table.bar import TableBarAlpaca
-from tests.utils.factory.infra.api.alpaca import (
-    generate_bar_alpaca_list,
-    generate_barset_alpaca,
-)
+from infra.adapter.materia.bar.process import extract_bar_list_alpaca_api_from_barset
+from tests.utils.factory.infra.api.alpaca import generate_barset_alpaca
 
 
 def test_extract_bar_alpaca_list_api_from_barset():
