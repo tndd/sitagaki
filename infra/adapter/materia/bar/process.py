986aab4aebe4b8437743c505b5cbcd69b491d857
tau <36781951+tndd@users.noreply.github.com>
2024-09-24 19:57:59 +0900

    もうbarモデルの直接変換は不可能なのでここは削除。さよなら

--- infra/adapter/materia/bar/process.py
+++ infra/adapter/materia/bar/process.py
@@ -12,21 +12,3 @@ def extract_bar_list_alpaca_api_from_barset(barset: BarSetAlpacaApi) -> List[Bar
     BarSetの中からBarのリストを取り出す。
     """
     return next(iter(barset.data.values()))
-
-
-def convert_bar_list_alpaca_api_to_table(
-        bars_alpaca_api: List[BarAlpacaApi],
-        timeframe: Timeframe
-) -> List[TableBarAlpaca]:
-    """
-    alpacaのバーのリストをDBのバーのリストに変換する。
-
-    HACK: bar_alpaca_api -> bar_alpaca_tableへの変換過程
-        本来であれば、
-        "alpaca_api -> domain -> table"の順で変換が必要だが、
-        "alpaca_api -> table"という直接変換を行う形式となってしまっている。
-
-        たまたま、alpaca_apiのバーとdomainのバーは同じデータ構造をしているため、
-        このような変換が可能になっている。
-    """
-    pass
--- tests/infra/adapter/materia/bar/test_process.py
+++ tests/infra/adapter/materia/bar/test_process.py
@@ -20,10 +20,3 @@ def test_extract_bar_alpaca_list_api_from_barset():
     bars = extract_bar_list_alpaca_api_from_barset(barset)
     assert isinstance(bars, list)
     assert all(isinstance(bar, Bar) for bar in bars)
-
-
-def test_convert_bar_list_alpaca_api_to_table():
-    bar_alpaca_list = generate_bar_alpaca_list()
-    tbl_bars_alpaca = convert_bar_list_alpaca_api_to_table(bar_alpaca_list, Timeframe.DAY)
-    assert isinstance(tbl_bars_alpaca, list)
-    assert all(isinstance(tbl_bar, TableBarAlpaca) for tbl_bar in tbl_bars_alpaca)
