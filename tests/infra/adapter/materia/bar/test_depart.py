8ae3346ae285f88a45f2995187b3b049f92895b6
tau <36781951+tndd@users.noreply.github.com>
2024-09-25 01:41:20 +0900

    wip

--- tests/infra/adapter/materia/bar/test_depart.py
+++ tests/infra/adapter/materia/bar/test_depart.py
@@ -1,11 +1,13 @@
 from alpaca.data.timeframe import TimeFrame as TimeFrameAlpaca
 
-from domain.materia.bar.model import Timeframe
+from domain.materia.bar.model import Adjustment, Timeframe
 from infra.adapter.materia.bar.depart import (
-    depart_bar_list_to_peewee_table,
     depart_bar_to_peewee_table,
+    depart_chart_to_peewee_table_list,
     depart_timeframe_to_alpaca_api,
 )
+from infra.db.table.bar import TableBarAlpaca
+from tests.utils.factory.domain.materia.bar import generate_bar, generate_bar_list
 
 
 def test_depart_timeframe_to_alpaca_api():
@@ -17,10 +19,22 @@ def test_depart_timeframe_to_alpaca_api():
 
 
 def test_depart_bar_to_peewee_table():
-    # TODO: 実装
-    pass
+    bar = generate_bar()
+    bar_table = depart_bar_to_peewee_table(
+        bar,
+        symbol="AAPL",
+        timeframe=Timeframe.MIN,
+        adjustment=Adjustment.RAW
+    )
+    assert isinstance(bar_table, TableBarAlpaca)
 
 
-def test_depart_bar_list_to_peewee_table():
-    # TODO: 実装
-    pass
+def test_depart_chart_to_peewee_table_list():
+    """
+    変換確認
+        Chart -> PeeweeTable<List>
+    """
+    chart = generate_chart()
+    bar_list_table = depart_chart_to_peewee_table_list(chart)
+    assert isinstance(bar_list_table, list)
+    assert all(isinstance(bar_table, TableBarAlpaca) for bar_table in bar_list_table)
