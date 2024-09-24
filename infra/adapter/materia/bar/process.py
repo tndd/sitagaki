d6565dca19cd9319102d2b545aaef768fa0c8b34
tau <36781951+tndd@users.noreply.github.com>
2024-09-25 01:11:39 +0900

    process完了

--- infra/adapter/materia/bar/process.py
+++ infra/adapter/materia/bar/process.py
@@ -3,9 +3,6 @@ from typing import List
 from alpaca.data.models import Bar as BarAlpacaApi
 from alpaca.data.models import BarSet as BarSetAlpacaApi
 
-from domain.materia.bar.model import Timeframe
-from infra.db.table.bar import TableBarAlpaca
-
 
 def extract_bar_list_alpaca_api_from_barset(barset: BarSetAlpacaApi) -> List[BarAlpacaApi]:
     """
