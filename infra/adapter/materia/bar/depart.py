b993267511fc321d3bc69c7ff94fcd393d745812
tau <36781951+tndd@users.noreply.github.com>
2024-09-25 01:38:11 +0900

    wip

--- infra/adapter/materia/bar/depart.py
+++ infra/adapter/materia/bar/depart.py
@@ -3,7 +3,7 @@ from typing import List
 from alpaca.data.timeframe import TimeFrame as TimeFrameAlpaca
 from alpaca.data.timeframe import TimeFrameUnit
 
-from domain.materia.bar.model import Bar, Timeframe
+from domain.materia.bar.model import Adjustment, Bar, Chart, Timeframe
 from infra.db.table.bar import TableBarAlpaca
 
 
@@ -28,21 +28,27 @@ def depart_timeframe_to_alpaca_api(timeframe: Timeframe) -> TimeFrameAlpaca:
     return timeframe_map[timeframe]
 
 
-def depart_bar_to_peewee_table(bar: Bar, timeframe: Timeframe) -> TableBarAlpaca:
+def depart_bar_to_peewee_table(
+        bar: Bar,
+        symbol: str,
+        timeframe: Timeframe,
+        adjustment: Adjustment
+) -> TableBarAlpaca:
     """
     Bar:
         Domain -> Peewee Table
+
+    これは基本的に外部からは使われることはない。
     """
     # TODO: 実装
     pass
 
 
-def depart_bar_list_to_peewee_table(
-        bars: List[Bar],
-        timeframe: Timeframe
-) -> List[TableBarAlpaca]:
+def depart_chart_to_peewee_table_list(chart: Chart) -> List[TableBarAlpaca]:
     """
-    Bar<List>:
-        Domain -> Peewee Table
+    Chart -> Peewee Table<List>
     """
-    return [depart_bar_to_peewee_table(bar, timeframe) for bar in bars]
+    return [
+        depart_bar_to_peewee_table(bar, chart.symbol, chart.timeframe, chart.adjustment)
+        for bar in chart.bars
+    ]
