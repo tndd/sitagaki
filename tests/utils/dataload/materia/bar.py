41a892a2b57c40c91773b8d5f791708042fca299
tau <36781951+tndd@users.noreply.github.com>
2024-09-26 20:05:45 +0900

    dataload prepare_test_barsの仮実装

---
+++ tests/utils/dataload/materia/bar.py
@@ -0,0 +1,12 @@
+def prepare_test_bars_on_db(
+        timeframe: Timeframe
+) -> None:
+    """
+    テスト用のbarデータをDBに登録する。
+
+    データはtimeframeで指定した先のテーブルに登録される。
+    登録内容については、generate_bar_list()の内容を参照。
+
+    TODO: 実装
+    """
+    pass
