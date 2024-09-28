da162837334adc86573190c641ba249127dd3320
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-09-28 16:30:27 +0900

    rename

--- tests/factory/infra/db/peewee/table/test_bar.py
+++ tests/factory/infra/db/peewee/table/test_bar.py
@@ -5,14 +5,12 @@ from tests.utils.factory.infra.db.peewee.bar import (
 )
 
 
-def test_generate_tbl_bar_base():
-    # TODO: rename
+def test_generate_table_bar_alpaca():
     table_bar_alpaca = generate_table_bar_alpaca()
     assert isinstance(table_bar_alpaca, TableBarAlpaca)
 
 
-def test_generate_tbl_bar_base_list():
-    # TODO: rename
+def test_generate_table_bar_alpaca_list():
     table_bar_alpaca_list = generate_table_bar_alpaca_list()
     assert isinstance(table_bar_alpaca_list, list)
     assert all(isinstance(table_bar_alpaca, TableBarAlpaca) for table_bar_alpaca in table_bar_alpaca_list)
