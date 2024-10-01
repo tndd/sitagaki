b29fb086a17faf441188a106fc0001514cac921a
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-01 18:47:00 +0900

    alpaca api test_barの分離

---
+++ tests/infra/api/alpaca/bar/test_get_bar_alpaca_api_list.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from alpaca.data.models import Bar
+from alpaca.data.requests import Adjustment as AdjustmentAlpaca
+from alpaca.data.timeframe import TimeFrame as TimeFrameAlpaca
+
+from infra.api.alpaca.bar import get_bar_alpaca_api_list
+
+
+def test_mock_get_bar_alpaca_api_list(replace_with_mock_get_barset_alpaca_api):
+    """
+    通信部分をモックにした簡易テスト
+    """
+    bar_alpaca_api_list = get_bar_alpaca_api_list(
+        symbol='AAPL',
+        start=datetime(2024,1,1),
+        timeframe=TimeFrameAlpaca.Day,
+        adjustment=AdjustmentAlpaca.RAW
+    )
+    assert isinstance(bar_alpaca_api_list, list)
+    assert all(isinstance(bar, Bar) for bar in bar_alpaca_api_list)
+
+
+def test_mock_get_bar_alpaca_api_list_empty_barset(replace_with_mock_get_barset_alpaca_api_fail_empty_barset):
+    """
+    存在しない条件を入力し、apiから空のBarSetが帰ってきた際の振る舞いのテスト
+    """
+    # Mock通信
+    bar_alpaca_api_list = get_bar_alpaca_api_list(
+        symbol='NOSYMBOL',
+        start=datetime(2024,1,1),
+        timeframe=TimeFrameAlpaca.Day,
+        adjustment=AdjustmentAlpaca.RAW
+    )
+    assert isinstance(bar_alpaca_api_list, list)
+    assert len(bar_alpaca_api_list) == 0
