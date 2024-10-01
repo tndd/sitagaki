b13b07430bbffc1bfe612fdf75156f19ca95f543
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-01 20:09:26 +0900

    fix typo

--- domain/materia/finance/chart/service.py
+++ domain/materia/finance/chart/service.py
@@ -6,7 +6,7 @@ from domain.materia.finance.chart.repository import ChartRepository
 
 @dataclass
 class ChartService:
-    chat_repo: ChartRepository
+    chart_repo: ChartRepository
 
     def fetch_chart(
             self,
@@ -21,7 +21,7 @@ class ChartService:
         # まずデータを最新にする
         self.update_chart(symbol, timeframe, adjustment)
         # データの取得
-        return self.chat_repo.fetch_chart_from_local(symbol, timeframe, adjustment)
+        return self.chart_repo.fetch_chart_from_local(symbol, timeframe, adjustment)
 
 
     def update_chart(
@@ -37,6 +37,6 @@ class ChartService:
         DB上にある最新のtimestamp~可能な限り直近のデータ。
         """
         # 最新のtimestampを取得
-        latest_timestamp = self.chat_repo.fetch_latest_timestamp_of_symbol(symbol, timeframe, adjustment)
+        latest_timestamp = self.chart_repo.fetch_latest_timestamp_of_symbol(symbol, timeframe, adjustment)
         # それ以降のデータで更新
-        self.chat_repo.store_chart_from_online(symbol, timeframe, adjustment, latest_timestamp)
+        self.chart_repo.store_chart_from_online(symbol, timeframe, adjustment, latest_timestamp)
