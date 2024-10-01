2a6ee97fdd0f00ff666749d3f2052ed1b1389480
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-01 17:20:28 +0900

    fix comment

--- tests/utils/fixture/infra/db/table/test_bar.py
+++ tests/utils/fixture/infra/db/table/test_bar.py
@@ -9,5 +9,5 @@ def test_prepare_table_bar_alpaca_on_db(
     BarデータがDBに登録されることを確認する。
     """
     result = TableBarAlpaca.select()
-    # ファクトリのBar本数は6本
+    # ファクトリのBar本数は10本
     assert len(result) == 10
