b20865ad85a6fb3a95ad6f165c1eda68596c5527
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-02 23:06:19 +0900

    まず雛形を作る

--- domain/materia/stock/ticker/model.py
+++ domain/materia/stock/ticker/model.py
@@ -1,6 +1,53 @@
-from uuid import UUID
+from enum import Enum
 
 from pydantic import BaseModel
+
+
+class Dow(Enum):
+    AMZN = "Amazon.com Inc"
+    AXP = "American Express Co"
+    AMGN = "Amgen Inc"
+    AAPL = "Apple Inc"
+    BA = "Boeing Co"
+    CAT = "Caterpillar Inc"
+    CSCO = "Cisco Systems Inc"
+    CVX = "Chevron Corp"
+    GS = "Goldman Sachs Group Inc"
+    HD = "Home Depot Inc"
+    HON = "Honeywell International Inc"
+    IBM = "International Business Machines Corp"
+    INTC = "Intel Corp"
+    JNJ = "Johnson & Johnson"
+    KO = "Coca-Cola Co"
+    JPM = "JPMorgan Chase & Co"
+    MCD = "McDonald’s Corp"
+    MMM = "3M Co"
+    MRK = "Merck & Co Inc"
+    MSFT = "Microsoft Corp"
+    NKE = "Nike Inc"
+    PG = "Procter & Gamble Co"
+    TRV = "Travelers Companies Inc"
+    UNH = "Unitedhealth Group Inc"
+    CRM = "Salesforce Inc"
+    VZ = "Verizon Communications Inc"
+    V = "Visa Inc"
+    WMT = "Walmart Inc"
+    DIS = "Walt Disney Co"
+    DOW = "Dow Inc"
+
+
+class Sector(Enum):
+    XLC = "Communication Services"
+    XLY = "Consumer Discretionary"
+    XLP = "Consumer Staples"
+    XLE = "Energy"
+    XLF = "Financials"
+    XLV = "Health Care"
+    XLI = "Industrials"
+    XLB = "Materials"
+    XLRE = "Real Estate"
+    XLK = "Technology"
+    XLU = "Utilities"
 
 
 class Ticker(BaseModel):
--- domain/materia/stock/ticker/repository.py
+++ domain/materia/stock/ticker/repository.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 
-from infra.db.sqlmodel import SQLModelClient
-
+from domain.materia.stock.ticker.model import Sector, Ticker
 from infra.api.alpaca.trade import get_assets
+from infra.db.peewee import PeeweeClient
 
 
 @dataclass
 class TickerRepository:
-    cli_db: SQLModelClient
+    cli_db: PeeweeClient
 
     def pull_tickers_from_online(self) -> None:
         """
@@ -17,3 +17,21 @@ class TickerRepository:
         # LATER: ticker repository 続きの実装
         # tickers_alpacaをドメインモデルに変換
         # tickersの保存
+        pass
+
+    def fetch_tickers_from_local(self) -> list[Ticker]:
+        """
+        全ての株式のリストをDBから取得する。
+        """
+        pass
+
+    def fetch_tickers_of_sector_from_local(self) -> list[str]:
+        """
+        WARN: 臨時実装
+
+        セクターの株式リストをDBから取得する。
+        """
+        return [
+            sector.key
+            for sector in Sector
+        ]
