844120e7b7f9817389b1c1d49cfdfc6e669ecaed
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-03 21:04:54 +0900

    新方式のalchemyクラス

--- infra/db/alchemy/table/bar.py
+++ infra/db/alchemy/table/bar.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from enum import Enum
+from typing import Optional
 
-from sqlalchemy import Column, DateTime, Enum, Float, String
-
-from infra.db.alchemy.client import Base
+from sqlalchemy import DateTime, Enum, Float, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class TimeframeTable(Enum):
@@ -19,17 +20,18 @@ class AdjustmentTable(Enum):
     ALL = 8
 
 
-class TableBarAlpaca(Base):
-    __tablename__ = "bar_alpaca"
 
-    timestamp = Column(DateTime, primary_key=True)
-    symbol = Column(String, primary_key=True)
-    timeframe = Column(Enum(TimeframeTable), primary_key=True)
-    adjustment = Column(Enum(AdjustmentTable), primary_key=True)
-    open = Column(Float)
-    high = Column(Float)
-    low = Column(Float)
-    close = Column(Float)
-    volume = Column(Float)
-    trade_count = Column(Float, nullable=True)
-    vwap = Column(Float, nullable=True)
+class TableBarAlpaca(DeclarativeBase):
+    __tablename__ = "alpaca_bar"
+
+    timestamp: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
+    symbol: Mapped[str] = mapped_column(String, primary_key=True)
+    timeframe: Mapped[TimeframeTable] = mapped_column(Enum(TimeframeTable), primary_key=True)
+    adjustment: Mapped[AdjustmentTable] = mapped_column(Enum(AdjustmentTable), primary_key=True)
+    open: Mapped[float] = mapped_column(Float)
+    high: Mapped[float] = mapped_column(Float)
+    low: Mapped[float] = mapped_column(Float)
+    close: Mapped[float] = mapped_column(Float)
+    volume: Mapped[float] = mapped_column(Float)
+    trade_count: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    vwap: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
