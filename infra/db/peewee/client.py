564064a3b6d966d0e2da4eee707ee44cc57a0875
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-04 18:47:10 +0900

    later追加

--- infra/db/peewee/client.py
+++ infra/db/peewee/client.py
@@ -2,16 +2,22 @@ from typing import List
 
 from peewee import Model, SqliteDatabase, chunked
 
-# DB接続
-DB = SqliteDatabase(':memory:')
-DB.connect()
+# DB設定
+"""
+LATER: DB接続先
+    今のところは強制的にSQLiteを使用する設定にしている。
+    将来的には接続先をテスト、本番によって変更できるようにする。
+"""
+_DB = SqliteDatabase(':memory:')
+
 
 # テーブル基底クラス
 class PeeweeTable(Model):
     class Meta:
-        database = DB
+        database = _DB
 
 
+# DB操作メソッド
 def insert_models(
     models: List[Model],
     batch_size: int = 10000,
@@ -26,10 +32,10 @@ def insert_models(
     TModel = type(models[0])
     # テーブルが存在しない場合にテーブルを作成
     if not TModel.table_exists():
-        DB.create_tables([TModel])
+        _DB.create_tables([TModel])
     # モデルをデータベースに挿入
     data = [model.__data__ for model in models]
-    with DB.atomic():
+    with _DB.atomic():
         for batch in chunked(data, batch_size):
             TModel.replace_many(batch).execute()
 
@@ -52,7 +58,7 @@ def truncate_tables(keyword: str):
     if keyword != 'TRUNCATE_TEST_TABLES':
         raise ValueError("keywordが異なるため、truncate_tablesを実行できません。")
 
-    tables = DB.get_tables()
-    with DB.atomic():
+    tables = _DB.get_tables()
+    with _DB.atomic():
         for table in tables:
-            DB.execute_sql(f"TRUNCATE TABLE {table}")
+            _DB.execute_sql(f"TRUNCATE TABLE {table}")
