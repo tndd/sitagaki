6471bf6b85d692a4b351c394514388bd81bb05ae
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-09 01:32:33 +0900

    rm

--- tests/infra/db/peewee/query/materia/stock/chart/test_get_query_select_latest_timestamp_of_bar_alpaca.py
+++ tests/infra/db/peewee/query/materia/stock/chart/test_get_query_select_latest_timestamp_of_bar_alpaca.py
@@ -6,8 +6,4 @@ from infra.db.peewee.query.materia.stock.chart import (
 
 def test_default():
     # TODO: テスト完成
-    query = get_query_select_latest_timestamp_of_bar_alpaca(
-        symbol="AAPL",
-        timeframe=Timeframe.DAY,
-        adjustment=Adjustment.RAW
-    )
+    pass
