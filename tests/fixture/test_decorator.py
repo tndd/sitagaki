0dab5e66a5bc44925e103cb24b0ba376b3c7fd4a
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-12 03:17:24 +0900

    printの比較追加

--- tests/fixture/test_decorator.py
+++ tests/fixture/test_decorator.py
@@ -2,12 +2,13 @@ import pytest
 
 import src.infra.db.common
 from fixture.decorator import only_test
-from src.infra.db.common import WorkMode
+from src.infra.db.common import CURRENT_WORK_MODE, WorkMode, is_test_mode
 
 print(f"Contents of src.infra.db.common: {dir(src.infra.db.common)}")
 print(f"CURRENT_WORK_MODE in src.infra.db.common: {'CURRENT_WORK_MODE' in dir(src.infra.db.common)}")
 if 'CURRENT_WORK_MODE' in dir(src.infra.db.common):
     print(f"Value of CURRENT_WORK_MODE: {src.infra.db.common.CURRENT_WORK_MODE}")
+    print(f"Value of CURRENT_WORK_MODE(from): {CURRENT_WORK_MODE}")
 
 
 @pytest.mark.parametrize(
@@ -23,8 +24,13 @@ def test_only_test(mocker, work_mode):
         "src.infra.db.common.CURRENT_WORK_MODE",
         work_mode
     )
+    print('=================')
     print(f"After patch - Value of CURRENT_WORK_MODE: {src.infra.db.common.CURRENT_WORK_MODE}")
+    print(f"After patch - Value of CURRENT_WORK_MODE(from): {CURRENT_WORK_MODE}")
     # CURRENT_WORK_MODE = work_mode # HACK: これを追加するとなぜかテスト期待通りの動作となる
+    print('=================')
+    print(f"is_test_mode: {src.infra.db.common.is_test_mode()}")
+    print(f"is_test_mode(from): {is_test_mode()}")
     # ワークモードが変更されているかを確認
     assert work_mode is src.infra.db.common.CURRENT_WORK_MODE
     # デコレータ検証用関数
