5b5434e1c6a404eeccde7b68a702277bce386fb7
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-12 02:25:37 +0900

    test_commonのtodo追加

---
+++ tests/fixture/factory/infra/db/test_common.py
@@ -0,0 +1,12 @@
+
+from src.infra.db.common import is_test_mode
+
+
+def test_read_work_mode_from_env():
+    # TODO: 無指定状態でIN_MEMORYが返ること、そして全てのパターンのテスト
+    pass
+
+
+def test_is_test_mode():
+    # TODO: パターンテスト
+    pass
