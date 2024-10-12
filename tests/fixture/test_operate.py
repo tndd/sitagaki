f2febbf0250759bbf86238b2c7e9b16eea9beb3f
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-12 18:13:00 +0900

    test_cleanup_tablesの実装

--- tests/fixture/test_operate.py
+++ tests/fixture/test_operate.py
@@ -1,10 +1,21 @@
-from fixture.operate import load_table_bar_alpaca_on_db
+import pytest
+from peewee import OperationalError
+
+from fixture.operate import cleanup_tables, load_table_bar_alpaca_on_db
 from src.infra.db.peewee.table.alpaca.bar import TableBarAlpaca
 
 
 def test_cleanup_tables():
-    # TODO: テーブルを空にできることを確認
-    pass
+    # テーブルにデータを登録し中身を確認
+    load_table_bar_alpaca_on_db()
+    result = TableBarAlpaca.select()
+    assert len(result) == 10
+    # テーブルを空にする
+    cleanup_tables()
+    result = TableBarAlpaca.select()
+    # テーブルが消去されているので、件数の取得に失敗する
+    with pytest.raises(OperationalError):
+        assert len(result) == 0
 
 
 def test_load_table_bar_alpaca_on_db():
