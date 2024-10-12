c37464d6061feb7cfb59f0b70993a93afb902958
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-12 21:28:00 +0900

    test_read_work_mode_from_envの実装

--- tests/fixture/factory/infra/db/test_common.py
+++ tests/fixture/factory/infra/db/test_common.py
@@ -1,12 +1,32 @@
 
+from os import environ
+
 import pytest
 
-from src.infra.db.common import WorkMode, is_test_mode
+from src.infra.db.common import WorkMode, is_test_mode, read_work_mode_from_env
 
 
-def test_read_work_mode_from_env():
-    # TODO: 無指定状態でIN_MEMORYが返ること、そして全てのパターンのテスト
-    pass
+@pytest.mark.parametrize(
+    "work_mode_env",
+    ["TEST", "DEV", "PROD", "IN_MEMORY", "INVALID", ""]
+)
+def test_read_work_mode_from_env(mocker, work_mode_env):
+    # 初期状態ではIN_MEMORYであることを確認
+    assert read_work_mode_from_env() == WorkMode.IN_MEMORY
+    # 環境変数をパッチ
+    mocker.patch.dict(
+        "os.environ",
+        {"WORK_MODE": work_mode_env}
+    )
+    # 環境変数が上書きされたことを確認
+    assert work_mode_env == environ["WORK_MODE"]
+    # ワークモードに登録されている文字列であれば正常に実行され、対応するWorkModeが返る
+    if work_mode_env in [mode.value for mode in WorkMode]:
+        assert read_work_mode_from_env() == WorkMode[work_mode_env]
+    else:
+        # 未登録の環境変数であれば、ValueErrorが発生する
+        with pytest.raises(ValueError):
+            read_work_mode_from_env()
 
 
 @pytest.mark.parametrize(
