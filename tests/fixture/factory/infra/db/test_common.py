4b598b531b6f2616e1de9ff4eb201f59669d3ed7
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-12 21:16:33 +0900

    test_is_test_modeの実装
    test_only_testとほとんど内容が変わらない

--- tests/fixture/factory/infra/db/test_common.py
+++ tests/fixture/factory/infra/db/test_common.py
@@ -1,5 +1,7 @@
 
-from src.infra.db.common import is_test_mode
+import pytest
+
+from src.infra.db.common import WorkMode, is_test_mode
 
 
 def test_read_work_mode_from_env():
@@ -7,6 +9,19 @@ def test_read_work_mode_from_env():
     pass
 
 
-def test_is_test_mode():
-    # TODO: パターンテスト
-    pass
+@pytest.mark.parametrize(
+    "work_mode",
+    list(WorkMode)
+)
+def test_is_test_mode(mocker, work_mode):
+    # 初期状態ではテストモードであることを確認
+    assert is_test_mode()
+    # パッチ適用
+    mocker.patch(
+        "src.infra.db.common.CURRENT_WORK_MODE",
+        work_mode
+    )
+    if work_mode in (WorkMode.TEST, WorkMode.IN_MEMORY):
+        assert is_test_mode()
+    else:
+        assert not is_test_mode()
