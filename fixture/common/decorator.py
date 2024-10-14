d4ec557cbd29fc3acc659c1604e5473e11744288
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-10-14 16:18:18 +0900

    factory用のdb投入デコレータの仮追加

--- fixture/common/decorator.py
+++ fixture/common/decorator.py
@@ -1,6 +1,7 @@
 from decorator import decorator
 
 from src.infra.db.common import is_test_mode
+from src.infra.db.peewee.client import CLI_PEEWEE, PeeweeTable
 
 
 @decorator
@@ -11,3 +12,17 @@ def only_test(f, *args, **kwargs):
     if not is_test_mode():
         raise ValueError("テストモードではないため、実行できません。 EID:019d3665")
     return f(*args, **kwargs)
+
+
+@decorator
+def auto_insert(f, enable=False, *args, **kwargs):
+    """
+    ファクトリにより作成されたモデルをDBに自動で登録するデコレータ。
+    ON,OFFを切り替え可能。
+    """
+    models: list[PeeweeTable] | PeeweeTable = f(*args, **kwargs)
+    if not isinstance(models, list):
+        models = [models]
+    if enable:
+        CLI_PEEWEE.insert_models(models)
+    return models
