85fa0406519dadea4a13d8c5cbda6444c9d8d561
tau <36781951+tndd@users.noreply.github.com>
2024-11-04 22:45:38 +0900

    arrive_symbol_timestamp_ls_from_tableのsymbols=Noneを受け取れるように修正

--- src/infra/adapter/origin/alpaca/bar/symbol_timestamp.py
+++ src/infra/adapter/origin/alpaca/bar/symbol_timestamp.py
@@ -3,24 +3,34 @@ from src.infra.db.peewee.table.alpaca.bar import TableBarAlpaca
 
 
 def arrive_symbol_timestamp_ls_from_table(
-    symbols: list[str],
     tables: list[TableBarAlpaca],
+    symbols: list[str] | None = None,
 ) -> list[SymbolTimestamp]:
     """
     テーブルモデルリストをシンボルと日時の情報のみを抜き出し、
     SymbolTimestampドメインモデルを生成する。
 
-    テーブルリストに渡されたシンボルが存在しない場合は、
-    timestamp=NoneとしたSymbolTimestampに変換する。
+    > テーブルリストに渡されたシンボルが存在しない場合
+        timestamp=NoneとしたSymbolTimestampに変換する。
 
-    NOTE: list[SymbolTimestamp]の順序
+    > 戻り値のlist[SymbolTimestamp]の順序
         symbolsの順番で帰る。
+
+    > symbols未指定時の戻り値の順番
+        tablesの順番
+
+    > symbols未指定時の動作
+        timestamp=Noneという変換は当然全く行われない
 
     NOTE: 存在しないsymbolのtimestampにNoneを入れる処理について
         本当はクエリの方でleft joinした結果を返しておけば話が早いんだが、
         クエリビルダーだけではそれが難しく、ややこしそうなのでこちらで処理する。
     """
     timestamp_map = {table.symbol: table.timestamp for table in tables}
+    # symbolsがNoneの場合、とっととtablesをそのままlist[SymbolTimestamp]変換して返す
+    if symbols is None:
+        return [SymbolTimestamp(symbol=table.symbol, timestamp=table.timestamp) for table in tables]
+    # symbols指定時、timestamp=Noneへの置き換え処理を行う
     symbol_timestamps = [
         SymbolTimestamp(
             symbol=symbol,
--- tests/src/infra/adapter/origin/alpaca/bar/test_symbol_timestamp.py
+++ tests/src/infra/adapter/origin/alpaca/bar/test_symbol_timestamp.py
@@ -7,6 +7,9 @@ from src.infra.adapter.origin.alpaca.bar.symbol_timestamp import (
 
 
 def test_basic():
+    """
+    存在しないシンボルについてはきちんとtimestamp=Noneで埋められてるかを確認
+    """
     # AAPL,GOOGのtable
     tables = factory_table_bar_alpaca_latest_timestamps()
     # 存在しないXXXX,YYYYも指定
@@ -23,3 +26,22 @@ def test_basic():
     # XXXX,YYYYのtimestampはNone
     assert symbol_timestamps[3].timestamp is None
     assert symbol_timestamps[4].timestamp is None
+
+
+def test_ommit_symbols():
+    """
+    symbolsの指定を省略した場合、
+    変換が期待通り行われていることを確認
+    """
+    # AAPL,GOOGのtable
+    tables = factory_table_bar_alpaca_latest_timestamps()
+    # symbolsを省略
+    symbol_timestamps = arrive_symbol_timestamp_ls_from_table(
+        tables=tables
+    )
+    # ARQ,BAL,ALM3件分のデータ
+    assert len(symbol_timestamps) == 3
+    # その３件のデータは本当にARQ,BAL,ALMか？
+    assert symbol_timestamps[0].symbol == "ARQ"
+    assert symbol_timestamps[1].symbol == "BAL"
+    assert symbol_timestamps[2].symbol == "ALM"
