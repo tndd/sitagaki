0292ad1b2f681dd72c72244de519044a7115c19d
tau <36781951+tndd@users.noreply.github.com>
2024-11-07 22:06:41 +0900

    ログ機能をクラス化
    クラス化する意味は若干乏しい。
    けどこれをインポートするだけでログ機能が使えるというのは機能としてシンプル。

--- log/client.py
+++ log/client.py
@@ -2,19 +2,46 @@ from datetime import datetime
 
 from loguru import logger
 
-# JSON形式でログを保存するための設定
-log_name = f"log/data/{datetime.now().strftime('%Y/%m/%d/%H:%M:%S:%f')}.log"
-logger.add(
-    log_name,
-    format="{time:YYYY-MM-DD at HH:mm:ss} |{level} | {file.path} | {function} | {message} | {extra}",
-    encoding='utf-8'
-)
 
-def main_f():
-    # 辞書型の情報を含むログメッセージの出力
-    payload = {"user": "example_user", "action": "login"}
-    logger.bind(**payload).debug('これはデバッグメッセージです')
-    logger.bind(**payload).info('これは情報メッセージです')
-    logger.bind(**payload).error('これはエラーメッセージです')
+class LogClient:
+    def __init__(self):
+        self._configure_logger()
 
-main_f()
+    def info(self, message: str, payload: dict | None = None):
+        self._record_log("info", message, payload)
+
+    def debug(self, message: str, payload: dict | None = None):
+        self._record_log("debug", message, payload)
+
+    def warn(self, message: str, payload: dict | None = None):
+        self._record_log("warning", message, payload)
+
+    def err(self, message: str, payload: dict | None = None):
+        self._record_log("error", message, payload)
+
+    def _configure_logger(self, log_path=None):
+        """
+        ログの形式と保存場所を設定
+        """
+        if log_path is None:
+            log_path = f"log/data/{datetime.now().strftime('%Y/%m/%d/%H:%M:%S:%f')}.log"
+        logger.add(
+            log_path,
+            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {file.path} | {function} | {message} | {extra}",
+            encoding='utf-8'
+        )
+
+    def _record_log(self, level: str, message: str, payload: dict | None):
+        if payload is None:
+            payload = {}
+        log_method = getattr(logger, level)
+        log_method(message, **payload)
+
+
+CLI_LOG = LogClient()
+
+# 使用例
+CLI_LOG.info('これは情報メッセージです', {"user": "example_user", "action": "login"})
+CLI_LOG.debug('これはデバッグメッセージです', {"user": "example_user", "action": "login"})
+CLI_LOG.warn('これは警告メッセージです', {"user": "example_user", "action": "login"})
+CLI_LOG.err('これはエラーメッセージです', {"user": "example_user", "action": "login"})
