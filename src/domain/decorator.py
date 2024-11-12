5a4f63dd2a6d3c7ae9d36ccdd4cc103ec9c3425e
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-11-12 16:54:15 +0900

    変数名更新

--- src/domain/decorator.py
+++ src/domain/decorator.py
@@ -6,17 +6,19 @@ from log.client import LOG, build_payload
 @contextmanager
 def log_error(
     message: str,
-    extra: dict = {},
+    payload: dict = {},
 ):
     """
     失敗時に引数のメッセージとエラーの内容から、
     ログを作成し記録する。
+
+    payloadは値の記録が必要な場合に使う。
     """
     try:
         yield
     except Exception as e:
         payload = build_payload(
-            extra,
+            payload,
             exception=e
         )
     LOG.error(message, **payload)
